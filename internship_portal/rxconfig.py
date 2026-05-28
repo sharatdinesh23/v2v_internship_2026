fb2a9bef4ba96def17bc43c02711cc761a98b4a6
sharatdinesh23 <sharathdinesh23@gmail.com>
2026-05-29 01:21:03 +0530

    changes made to fix ninx

--- internship_portal/rxconfig.py
+++ internship_portal/rxconfig.py
@@ -7,8 +7,6 @@ api_url = os.getenv("REFLEX_API_URL", "http://127.0.0.1:8010")
 config = rx.Config(
     app_name="internship_portal",
     api_url=api_url,
-    backend_port=8010,
-    frontend_port=3000,
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
